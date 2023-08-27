b5aff234b271845dd5ced4a8bbc1c7732e1094e4
unknown <kwakuboachiefosu@gmail.com>
2023-08-27 23:33:54 +0000

    Working on responsive screens for md and above

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,16 +2,11 @@
 module.exports = {
   content: ["./public/*.{html,js}"],
   theme: {
-    // screens: {
-    //   sm: "480px",
-    //   md: "768px",
-    //   lg: "976px",
-    //   xl: "1440px",
-    // },
     extend: {
       fontFamily: {
         Raleway: ["Raleway"],
       },
+     
     },
   },
   plugins: [],
